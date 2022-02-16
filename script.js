17a40cf0c4fa84f75c89d0ee7eab7ffe348625ac
Allen Mathews <allenjoemathews@gmail.com>
2022-02-16 17:19:50 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let myLeads = ["a", "b", "c"]
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+})
+
+for (let i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i])
+}

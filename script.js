2f098e6dbdacf352f30eab70ab9f0fe9ff819011
Allen Mathews <allenjoemathews@gmail.com>
2022-02-24 21:11:45 +1100

    final

--- script.js
+++ script.js
@@ -4,15 +4,30 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-    // 1. Grab the SAVE TAB button and store it in a tabBtn variable
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
 
-// 2. Listen for clicks on tabBtn. Log Per's LinkedIn URL to the console
+const tabs = [
+    { url: "https://www.linkedin.com/in/per-harald-borgen/" }
+]
 
+
+tabBtn.addEventListener("click", function() {
+    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    // })
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+
+
+})
 
 function render(leads) {
     let listItems = ""

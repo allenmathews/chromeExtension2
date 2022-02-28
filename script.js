2efa0cffc2b507683f1ada321aaa09e3c89a080f
Allen Mathews <allenjoemathews@gmail.com>
2022-02-28 20:25:24 +1100

    removed comments

--- script.js
+++ script.js
@@ -17,9 +17,6 @@ const tabs = [
 
 
 tabBtn.addEventListener("click", function() {
-    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    // })
-
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         myLeads.push(tabs[0].url)
         localStorage.setItem("myLeads", JSON.stringify(myLeads))
